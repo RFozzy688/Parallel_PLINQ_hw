b617569c2d4f88b1c14d591f3a6856a8a46632d9
RFozzy688 <pokerfozzy@gmail.com>
2023-11-09 01:14:41 +0200

    чтение файла

--- task_6/Program.cs
+++ task_6/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace task_6
     {
         static void Main(string[] args)
         {
+            Program pr = new Program();
+            List<int> array = new List<int>();
+
+            pr.ReadFile(array);
+            foreach(int i in array)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        void ReadFile(List<int> arr)
+        {
+            using (FileStream fs = new FileStream(@"..\..\nums.txt", FileMode.Open))
+            {
+                using(StreamReader sr = new StreamReader(fs))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        arr.Add(Int32.Parse(sr.ReadLine()));
+                    }
+                }
+            }
         }
     }
 }
