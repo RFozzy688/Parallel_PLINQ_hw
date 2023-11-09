37bf79d24f2fa9543f43eba533dc1874e11edfb4
RFozzy688 <pokerfozzy@gmail.com>
2023-11-09 21:14:40 +0200

    поиск последовательности

--- task_6/Program.cs
+++ task_6/Program.cs
@@ -12,13 +12,28 @@ namespace task_6
         static void Main(string[] args)
         {
             Program pr = new Program();
-            List<int> array = new List<int>();
+            List<int> nums = new List<int>();
 
-            pr.ReadFile(array);
-            foreach(int i in array)
+            pr.ReadFile(nums);
+
+            foreach(int i in nums)
             {
                 Console.Write(i + " ");
             }
+
+            Console.WriteLine();
+
+
+            var longestSequenceLength = nums
+                .AsParallel()
+                .Aggregate(
+                    new { Max = 0, Current = 0, Previous = nums.Min() },
+                    (a, x) => x > a.Previous
+                        ? new { Max = a.Max > a.Current + 1 ? a.Max : a.Current + 1, Current = a.Current + 1, Previous = x }
+                        : new { Max = a.Max, Current = 0, Previous = nums.Min() })
+                .Max + 1;
+
+            Console.WriteLine($"Длина наибольшей возростающей последовательности: {longestSequenceLength}");
         }
         void ReadFile(List<int> arr)
         {
